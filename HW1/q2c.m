fe555f95e6978fd36379922338ed5f54c05023ce
unknown <hbassi21@berkeley.edu>
2022-01-28 10:03:57 -0800

    fixed plot

--- HW1/q2c.m
+++ HW1/q2c.m
@@ -3,7 +3,7 @@ u = @(x) sin(2*x);
 exact = -4*sin(2);
 errs = [];
 disp(' ')
-disp('       h                 err                 pred err')
+disp('       h                 err       pred err')
 for i=1:length(hvals)
     approx = (-8.333333333333333e-02*u(1-2*hvals(i)) + 1.333333333333333e+00*u(1-hvals(i)) + -2.500000000000000e+00*u(1) +  1.333333333333333e+00 * u(1+hvals(i)) +   -8.333333333333333e-02 * u(1+2*hvals(i)))/(hvals(i)^2);
     err = abs(approx-exact);
@@ -11,7 +11,7 @@ for i=1:length(hvals)
     fprintf('%13.4e   %13.4e   %13.4e\n', hvals(i) ,err, -0.0111111 * hvals(i)^4*-64*sin(2))
 end
 
-loglog(hvals, errs)
+loglog(hvals, errs,'-o')
 hold on 
 sl4 = @(x) x.^(4);
 ys4 = sl4(hvals);
@@ -19,5 +19,5 @@ loglog(hvals, ys4);
 xlabel('h')
 ylabel('error')
 title('loglog error vs h')
-legend('FD error', 'slope -4')
+legend('FD error', 'slope 4')
 saveas(gcf,'2c.png')
